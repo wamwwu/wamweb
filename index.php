ca36bbaff8ca27d6edc6b25b70d7edc55661bfbf
wamwwu <wwu.wam@gmail.com>
2016-09-25 22:36:29 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+
+
+<head>
+	
+<meta charset ="utf-8">
+	<title> </title>
+	
+<meta name="author" content= "">
+	
+
+</head>
+
+
+
+<body>
+
+	
+<p> Hello, world! The year is <?php echo date(Y); ?>. </p>
+
+
+</body>
+
+
+
+
+</html>
